fb442927c3acadfd30b97a31c0dcc44474377a05
Juan Andonaire <jandonairevi2015@fau.edu>
2018-05-04 12:46:53 -0400

    Update lcd_humiditytemp_firebase.py

--- lcd_humiditytemp_firebase.py
+++ lcd_humiditytemp_firebase.py
@@ -1,7 +1,7 @@
-import i2c_LCD_driver
+import i2c_LCD_driver #library for the lcd
 import sys
-import Adafruit_DHT
-from firebase import firebase
+import Adafruit_DHT #library for the humidity and temperature sensor
+from firebase import firebase #library for firebase
 
 import urllib2, urllib, httplib
 import json
@@ -12,19 +12,20 @@ from time import *
 
 mylcd = i2c_LCD_driver.lcd()
 
-#we insert a link provided in firebase
+#use the link provide by firebase so we can store our data within the firebase database
 firebase = firebase.FirebaseApplication('https://myapp-67d2e.firebaseio.com/', None)
 
+#this function will help us to display the updated changes that the sensor will send
 def update_firebase():
 
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-        mylcd.lcd_display_string("Temp: %d C" % temperature, 1, 0)
-        mylcd.lcd_display_string("Humidity: %d.0 %%" % humidity, 2, 0)
+        mylcd.lcd_display_string("Temp: %d C" % temperature, 1, 0) #value of the temperature will be displayed in the first row of the lcd
+        mylcd.lcd_display_string("Humidity: %d.0 %%" % humidity, 2, 0) #value of the humidity will be displayed in the second row of the lcd
         print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
 
-        data = {"Temp": temperature, "Humidity": humidity}
-        firebase.post('/sensor/humiditytemp', data)
+        data = {"Temp": temperature, "Humidity": humidity} #set variable data to the humidity and temperature values
+        firebase.post('/sensor/humiditytemp', data) #the values will be posted in firebase database 
 
 while True:
-        update_firebase()
+        update_firebase() #the values will be updated every 20 seconds, however time can be modified
         sleep(20)
